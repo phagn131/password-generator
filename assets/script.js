15ba1379f8fbee7465da80b6b34688c36254fe2c
phagn131 <97428101+phagn131@users.noreply.github.com>
2022-09-27 18:46:01 -0400

    "reset code path"

--- assets/script.js
+++ assets/script.js
@@ -2,38 +2,16 @@
 // adding various methods, variables from class to jumpstart assignment
 //document.body;
 //console.log(main);
-// Get references to the #generate element
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-//console.log("my name is paul");
-
-//function generatePassword(){
- window.prompt("Please choose the criteria for your password")
-//}
-// 
-//var password=document.getElementById("password");
-//var numbers = "0123456789";
-//var specialCharachters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-//var passwordLength = 8-128 
-
-///console.log(testconsole);
-
-//charachter length
-//for (var i = 0; i < password.length; i++){
-//var randomNumber = Math.floor(parseInt(userNumber))(Math.random() * options.length);
-//}
-// Get references to the #generate element
-//var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-//function writePassword() {
-//  var password = generatePassword();
-//  var passwordText = document.querySelector("#password");
-//
-//  passwordText.value = password;
-//  var index = Math.floor(Math.random() * options.length);
-//}
+//write password to the assignment input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
