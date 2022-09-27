ab0d0e1345505bdb94a72bb30b8c1ba3b3edc7bf
phagn131 <97428101+phagn131@users.noreply.github.com>
2022-09-27 18:13:54 -0400

    "corrected link, setting win prompts"

--- assets/script.js
+++ assets/script.js
@@ -3,34 +3,37 @@
 //document.body;
 //console.log(main);
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-function generatePassword(){
-  window.prompt("Please choose the criteria for your password")
-}
-// 
-var password=document.getElementById("password");
-var numbers = "0123456789";
-var specialCharachters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-var passwordLength = 8-128 
-
-console.log(testconsole);
-
-//charachter length
-for (var i = 0; i < password.length; i++){
-var randomNumber = Math.floor(parseInt(userNumber))(Math.random() * options.length);
-}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//console.log("my name is paul");
 
-  passwordText.value = password;
-  var index = Math.floor(Math.random() * options.length);
-}
+//function generatePassword(){
+ window.prompt("Please choose the criteria for your password")
+//}
+// 
+//var password=document.getElementById("password");
+//var numbers = "0123456789";
+//var specialCharachters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//var passwordLength = 8-128 
+
+///console.log(testconsole);
+
+//charachter length
+//for (var i = 0; i < password.length; i++){
+//var randomNumber = Math.floor(parseInt(userNumber))(Math.random() * options.length);
+//}
+// Get references to the #generate element
+//var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+//function writePassword() {
+//  var password = generatePassword();
+//  var passwordText = document.querySelector("#password");
+//
+//  passwordText.value = password;
+//  var index = Math.floor(Math.random() * options.length);
+//}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
