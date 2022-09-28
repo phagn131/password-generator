dd4c2ebb5c9be1dfd04e7529d30fcb1ae56e1f6f
phagn131 <97428101+phagn131@users.noreply.github.com>
2022-09-28 00:01:29 -0400

    "added functions and alerts for invalid inputs"

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,28 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+ var userInput = window.prompt("Choose the password criteria by clicking ok");
+ console.log(userInput)  
+ var userInput = window.prompt("Type in a password length between 8 and 128 charachters?")
+ var passwordLength = parseInt(userInput)
+
+ if (isNaN(passwordLength)) {
+    window.alert("Must input number!")
+    return
+ } 
+
+ console.log(userInput)  
+ var userInput = window.prompt("Do you want to include lowercase letters?")
+ console.log(userInput)  
+ var userInput = window.prompt("Do you want to include uppercase letters?")
+ console.log(userInput)  
+ var userInput = window.prompt("Do you want to include numbers?")
+ console.log(userInput)  
+ var userInput = window.prompt("Do you want to include special charachters?")
+ console.log(userInput)
+
+}
 //write password to the assignment input
 function writePassword() {
     var password = generatePassword();
