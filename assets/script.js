38c9c2d92ca9ef2d176fee7e44e57d315fc00a78
phagn131 <97428101+phagn131@users.noreply.github.com>
2022-10-01 17:59:29 -0400

    "setting up criteria questions"

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,85 @@
-/* Assignment code here
-//set variables for each function (use var instead of let to run on older browsers)
-//use if else statements
-//math operator to generate random assignment to numbers, text and special charachters
-//use window.prompt for user input
-// Get references to the #generate element*/
+// Assignment code here
+
 var generateBtn = document.querySelector("#generate");
+
 
 function generatePassword(){
 
-//set variable for password length
-var userInput = window.prompt("How many charachters long do you want your password to be?") 
+var userInput = window.prompt("How many charachters long do you want your password to be?")
 
 var passwordLength = parseInt(userInput)
-//use not a number if user inputs invalid type
+ 
 if (isNaN(passwordLength)) {
     window.alert("Must input number!")
     return
 }
-    if (passwordLength < 8 || passwordLength > 128)
-    window.alert("Please enter a number between 8 and 128")
+    if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Please enter a number between 8 and 128 charachters")
     return
- }
-//add 4 prompt messages then add if else statements 
+}
+    if (passwordLength >= 8 && passwordLength < 128) {
+    window.alert("choose the following charachters for your password")
+    
+}
 
-var userSelectsLower = window.prompt("Would you like to include lowercase letters?")
-var userSelectsUpper = window.prompt("Would you like to include uppercase letters?")
-var userSelectsNumbers = window.prompt("Would you like to include numbers?")
-var userSelectsSpecialCharachters = window.prompt("Would you like to include special charachters?")
+    window.confirm("Would you like to include lowercase letters in your password? Press OK for yes or Cancel for no") 
 
-//set an array for the allowable input ***ADD quotes to array or find shortcut
-var lowerList = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
-var UpperList = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
-var NumbersList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var SpecialCharList = ["!", "#", "$", "%", "&","'", "(", ")", "*","+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?","@", "[", "\", "^",_`{|}~"]
+    window.confirm("Would you like to include uppercase letters in your password? Press OK for yes or Cancel for no")
+
+    window.confirm("Would you like to include numbers in your password? Press OK for yes or Cancel for no")
+
+    window.confirm("Would you like to include special charachters in your password? Press OK for yes or Cancel for no")
+}
+
+/*var userSelectsLower = window.confirm("Would you like to include lowercase letters in your password?")
+console.log(userSelectsLower)
+var userSelectsUpper = window.confirm("Would you like to include uppercase letters in your password?")
+
+var userSelectsNumbers = window.confirm("Would you like to include numbers in your password?")
+
+var userSelectsSpecialCharachters = window.confirm("Would you like to include special charachters in your password?") */
+var userSelectsLower
+var userSelectsUpper
+var userSelectsNumbers
+var userSelectsSpecialCharachters
+
+/*set an array for the allowable input ***ADD quotes to array or find shortcut
+var lowerList = ["a", "b", "c", "d", "e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var upperList = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X", "Y", "Z"]
+var numbersList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var specialCharList = ["!", "#", "$", "%", "&","'", "(", ")", "*","+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?","@", "["] 
+
+var choicesList = []
+
+if (userSelectsLower === true) {
+    choicesList.push(lowerList)
+}
+
+if (userSelectsUpper === true) {
+    choicesList.push(upperList)
+}
+
+if (userSelectsNumbers === true) {
+    choicesList.push(numbersList)
+}
+
+if (userSelectsLower === true) {
+    choicesList.push(lowerList)
+}
+
+if (choicesList === 0) {
+    choicesList.push(lowerList)
+}
+
+var generatePassword = ""
+
+for (var i = 0; i < passwordLength; i++) {
+    var randomChoice = getRandomItem(choicesList)
+    var randomChar = getRandomItem(randomChoice)
+    generatedPassword += randomChar
+}
+
+*/
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
@@ -40,4 +87,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword)
+generateBtn.addEventListener("click", writePassword);
