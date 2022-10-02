687bfedbd0714319aed0d972cfee39057caf3a20
phagn131 <97428101+phagn131@users.noreply.github.com>
2022-10-01 23:26:15 -0400

    "adding variables and trying to fix syntax errors"

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,19 @@
 
 var generateBtn = document.querySelector("#generate");
 
+function randomInt(min, max) {
+    if (!max) {
+        max = min
+        min = 0
+    }
+    var rand = Math.random()
+    return Math.floor(min*(1-rand) + rand*max)
+}
+
+function getRandomItem(list) {
+    return list[randomInt(list.length)]
+    console.log(randomInt)
+}
 
 function generatePassword(){
 
@@ -9,82 +22,72 @@ var userInput = window.prompt("How many charachters long do you want your passwo
 
 var passwordLength = parseInt(userInput)
  
-if (isNaN(passwordLength)) {
+if (isNaN(passwordLength)){ 
     window.alert("Must input number!")
     return
 }
-    if (passwordLength < 8 || passwordLength > 128) {
+if (passwordLength < 8 || passwordLength > 128) {
     window.alert("Please enter a number between 8 and 128 charachters")
     return
 }
-    if (passwordLength >= 8 && passwordLength < 128) {
+if (passwordLength >= 8 && passwordLength < 128) {
     window.alert("choose the following charachters for your password")
     
 }
 
-    window.confirm("Would you like to include lowercase letters in your password? Press OK for yes or Cancel for no") 
-
-    window.confirm("Would you like to include uppercase letters in your password? Press OK for yes or Cancel for no")
-
-    window.confirm("Would you like to include numbers in your password? Press OK for yes or Cancel for no")
-
-    window.confirm("Would you like to include special charachters in your password? Press OK for yes or Cancel for no")
-}
-
-/*var userSelectsLower = window.confirm("Would you like to include lowercase letters in your password?")
+var userSelectsLower = window.confirm("Would you like to include lowercase letters in your password? Press OK for yes or Cancel for no") 
 console.log(userSelectsLower)
-var userSelectsUpper = window.confirm("Would you like to include uppercase letters in your password?")
+var userSelectsUpper = window.confirm("Would you like to include uppercase letters in your password? Press OK for yes or Cancel for no")
+console.log(userSelectsUpper)
+var userSelectsNumbers = window.confirm("Would you like to include numbers in your password? Press OK for yes or Cancel for no")
+console.log(userSelectsNumbers)
+var userSelectsSpecialCharachters = window.confirm("Would you like to include special charachters in your password? Press OK for yes or Cancel for no")
+console.log(userSelectsSpecialCharachters)
 
-var userSelectsNumbers = window.confirm("Would you like to include numbers in your password?")
-
-var userSelectsSpecialCharachters = window.confirm("Would you like to include special charachters in your password?") */
-var userSelectsLower
-var userSelectsUpper
-var userSelectsNumbers
-var userSelectsSpecialCharachters
-
-/*set an array for the allowable input ***ADD quotes to array or find shortcut
 var lowerList = ["a", "b", "c", "d", "e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var upperList = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X", "Y", "Z"]
 var numbersList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var specialCharList = ["!", "#", "$", "%", "&","'", "(", ")", "*","+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?","@", "["] 
+var specialCharachtersList = ["!", "#", "$", "%", "&","'", "(", ")", "*","+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?","@", "["] 
 
-var choicesList = []
+var optionsCart = []
 
 if (userSelectsLower === true) {
-    choicesList.push(lowerList)
+    optionsCart.push(lowerList)
 }
 
 if (userSelectsUpper === true) {
-    choicesList.push(upperList)
+    optionsCart.push(upperList)
 }
 
 if (userSelectsNumbers === true) {
-    choicesList.push(numbersList)
+    optionsCart.push(numbersList)
 }
 
-if (userSelectsLower === true) {
-    choicesList.push(lowerList)
+if (userSelectsSpecialCharachters === true) {
+    optionsCart.push(specialCharachtersList)
 }
 
-if (choicesList === 0) {
-    choicesList.push(lowerList)
+if (optionsCart.length === 0) {
+    optionsCart.push(lowerList)
 }
 
-var generatePassword = ""
+var generatedPassword = ""
 
 for (var i = 0; i < passwordLength; i++) {
-    var randomChoice = getRandomItem(choicesList)
-    var randomChar = getRandomItem(randomChoice)
+    var randomList = getRandomItem(optionsCart)
+    var randomChar = getRandomItem(randomList)
     generatedPassword += randomChar
-}
 
-*/
+    return generatedPassword
+
+}
+}
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
-}
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
