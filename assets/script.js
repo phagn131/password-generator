e5b82ac2f24d480b56bea0c0b993d1355e44aab1
phagn131 <97428101+phagn131@users.noreply.github.com>
2022-10-02 23:10:46 -0400

    update

--- assets/script.js
+++ assets/script.js
@@ -9,14 +9,13 @@ function randomInt(min, max) {
     }
     var rand = Math.random()
     return Math.floor(min*(1-rand) + rand*max)
-}
+    }
 
 function getRandomItem(list) {
     return list[randomInt(list.length)]
-    console.log(randomInt)
-}
+    }
 
-function generatePassword(){
+function generatePassword() {
 
 var userInput = window.prompt("How many charachters long do you want your password to be?")
 
@@ -25,15 +24,11 @@ var passwordLength = parseInt(userInput)
 if (isNaN(passwordLength)){ 
     window.alert("Must input number!")
     return
-}
+    }
 if (passwordLength < 8 || passwordLength > 128) {
     window.alert("Please enter a number between 8 and 128 charachters")
     return
-}
-if (passwordLength >= 8 && passwordLength < 128) {
-    window.alert("choose the following charachters for your password")
-    
-}
+    }
 
 var userSelectsLower = window.confirm("Would you like to include lowercase letters in your password? Press OK for yes or Cancel for no") 
 console.log(userSelectsLower)
@@ -41,35 +36,36 @@ var userSelectsUpper = window.confirm("Would you like to include uppercase lette
 console.log(userSelectsUpper)
 var userSelectsNumbers = window.confirm("Would you like to include numbers in your password? Press OK for yes or Cancel for no")
 console.log(userSelectsNumbers)
-var userSelectsSpecialCharachters = window.confirm("Would you like to include special charachters in your password? Press OK for yes or Cancel for no")
-console.log(userSelectsSpecialCharachters)
+var userSelectsspecialCharachters = window.confirm("Would you like to include special charachters in your password? Press OK for yes or Cancel for no")
+console.log(userSelectsspecialCharachters)
 
 //set an array for the allowable input ***ADD quotes to array or find shortcut
 var lowerList = ["a", "b", "c", "d", "e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var upperList = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X", "Y", "Z"]
 var numbersList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var SpecialCharachtersList = ["!", "#", "$", "%", "&","'", "(", ")", "*","+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?","@", "["] 
+var specialList = ["!", "#", "$", "%", "&","'", "(", ")", "*","+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?","@", "["] 
+
 var optionsCart = []
 
 if (userSelectsLower === true) {
     optionsCart.push(lowerList)
-}
+    }
 
 if (userSelectsUpper === true) {
     optionsCart.push(upperList)
-}
+    }
 
 if (userSelectsNumbers === true) {
     optionsCart.push(numbersList)
-}
+    }
 
-if (userSelectsSpecialCharachters === true) {
-    optionsCart.push(SpecialCharachtersList)
-}
+if (userSelectsspecialCharachters === true) {
+    optionsCart.push(specialList)
+    }
 
 if (optionsCart === 0) {
     optionsCart.push(lowerList)
-}
+    }
 
 var generatedPassword = ""
 
@@ -77,15 +73,20 @@ for (var i = 0; i < passwordLength; i++) {
     var randomList = getRandomItem(optionsCart)
     var randomChar = getRandomItem(randomList)
     generatedPassword += randomChar
-}
-    return generatedPassword
+    }
+    
 
+return generatedPassword
+}
 
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
+    
     passwordText.value = password;
 }
 
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
