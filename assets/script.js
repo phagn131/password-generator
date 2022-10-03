7f0785643ffa7804ebb48e36eac085e04155aa35
phagn131 <97428101+phagn131@users.noreply.github.com>
2022-10-03 01:13:34 -0400

    "converting ascii with concat"

--- assets/script.js
+++ assets/script.js
@@ -40,17 +40,24 @@ var userSelectsspecialCharachters = window.confirm("Would you like to include sp
 console.log(userSelectsspecialCharachters)
 
 //set an array for the allowable input ***ADD quotes to array or find shortcut
-var lowerList = arrayFromLowToHigh(65, 90) //["a", "b", "c", "d", "e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var upperList = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X", "Y", "Z"]
-var numbersList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var specialList = ["!", "#", "$", "%", "&","'", "(", ")", "*","+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?","@", "["]
+const lowerList = arrayFromLowToHigh(65, 90) //["a", "b", "c", "d", "e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+const upperList = arrayFromLowToHigh(97, 122)//["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X", "Y", "Z"]
+const numbersList = arrayFromLowToHigh(48, 57)//["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const specialList = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123, 126))//["!", "#", "$", "%", "&","'", "(", ")", "*","+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?","@", "["]
+String.fromCharCode()
+let charCodes = lowerList
+if (upperList) charCodes = charCodes.concat
+(upperList)
+if (numbersList) charCodes = charCodes.concat(numbersList)
+if (specialList) charCodes = charCodes.concat(specialList)
 
-String.fromCharCode(65)
-console.log(String.fromCharCode(65, 85))
+//for (let i = 0; i < passwordLength, i++){
+
+//}
 
 function arrayFromLowToHigh(low, high){
-var array = []
-for (var i = low; i <= high; i++){
+const array = []
+for (let i = low; i <= high; i++){
   array.push(i)      
 }
 return array
